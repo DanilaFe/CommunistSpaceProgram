2044fbfcfdd6f8f93df147aced0873e3ffd3c347
Danila Fedorin <danila.fedorin@gmail.com>
2016-12-18 00:35:14 -0800

    Create bare bones ScriptNode class.

---
+++ Assets/Scripts/ScriptNode.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/**
+ *  A script node.
+ *  Represents nodes being placed by the user
+ *  to script a robot component.
+ **/
+public class ScriptNode {
+
+	/**
+	 *  Enum representing different node types.
+	 **/
+	public enum ScriptNodeType {
+		ROOT,
+		TURN_ON,
+		TURN_OFF
+	}
+		
+	/**
+	 * The child nodes of the node.
+	 **/
+	public List<ScriptNode> children;
+	/**
+	 * The type of the node.
+	 **/
+	public ScriptNodeType type;
+	/**
+	 * The data associated specifically with this node.
+	 **/
+	public Dictionary<string, Object> data;
+
+	/**
+	 * Creates a new ScriptNode of the given type.
+	 **/
+	public ScriptNode(ScriptNodeType nodeType){
+		children = new List<ScriptNode> ();
+		data = new Dictionary<string, Object> ();
+		type = nodeType;
+	}
+
+	/**
+	 * Adds a child node to the node.
+	 **/
+	public void addNode(ScriptNode newNode){
+		children.Add (newNode);
+	}
+
+	/**
+	 *  Removes a child node from the node.
+	 **/
+	public void removeNode(ScriptNode toRemove){
+		children.Remove (toRemove);
+	}
+}
