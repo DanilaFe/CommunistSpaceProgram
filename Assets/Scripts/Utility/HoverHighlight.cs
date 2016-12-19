6bd91766b3266b0d58b3269d1b2e943c0ad88c74
Danila Fedorin <danila.fedorin@gmail.com>
2016-12-19 14:46:28 -0800

    Add script to highlight object on hover.

---
+++ Assets/Scripts/Utility/HoverHighlight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Script to highlight objects that are being hovered over.
+ **/
+public class HoverHighlight : MonoBehaviour {
+
+	/**
+	 * The color this object should have when it's not being hovered over. 
+	 **/
+	public Color normalColor;
+	/**
+	 * The color this object should have when it is being hovered over. 
+	 **/
+	public Color hoverColor;
+	/**
+	 *  The SpriteRenderer that draws this object's sprite.
+	 **/
+	SpriteRenderer spriteRenderer;
+
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void OnMouseEnter() {
+		spriteRenderer.color = hoverColor;
+	}
+
+	void OnMouseExit(){
+		spriteRenderer.color = normalColor;
+	}
+}
