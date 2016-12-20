1a3ec3b39dac9e47992fed678d2150eb0c4d6ed1
Danila Fedorin <danila.fedorin@gmail.com>
2016-12-19 16:29:38 -0800

    Begin script to automatically manage / arrange RobotBuilderBoxes.

---
+++ Assets/Scripts/RobotBuilderArea.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotBuilderArea : MonoBehaviour {
+
+	RobotBuilderBox[,] robotBuilderBoxes;
+	public RobotBuilderBox builderBoxPrefab;
+	public int widthBoxes = 5;
+	public int heightBoxes = 5;
+
+	void Start () {
+		robotBuilderBoxes = new RobotBuilderBox[widthBoxes, heightBoxes];
+		for (int w = 0; w < widthBoxes; w++) {
+			for (int h = 0; h < heightBoxes; h++) {
+				RobotBuilderBox newBox = Instantiate (builderBoxPrefab);
+				robotBuilderBoxes [w, h] = newBox;
+			}
+		}
+	}
+
+	void Update () {
+	
+	}
+}
