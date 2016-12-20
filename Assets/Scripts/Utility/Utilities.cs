58ea1dca600d50b3eb6f3d1ced626ea4d66def61
Danila Fedorin <danila.fedorin@gmail.com>
2016-12-19 16:02:58 -0800

    Create a utility class and add MatchColliderSize function.

---
+++ Assets/Scripts/Utility/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Static utility functions go here
+ **/
+public class Utilities {
+
+	/**
+	 * Sets an object's collider's size to be its sprite size.
+	 **/
+	public static void MatchBoxColliderSize(GameObject obj){
+		SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer> ();
+		BoxCollider2D collider = obj.GetComponent<BoxCollider2D> ();
+		if (spriteRenderer != null && collider != null) {
+			collider.size = spriteRenderer.bounds.size;
+		}
+	}
+
+}
