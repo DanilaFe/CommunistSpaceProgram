1e7dfd33adc64cdf6d859761c60ddeb5b5ef18c4
U-BSD\452434 <452434@SHS-10D8A6871.beaverton.k12.or.us>
2017-01-19 16:28:44 -0800

    Write a script for dragging objects

---
+++ Assets/Scripts/Utility/Drag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    private bool dragging = false;
+    private Transform position;
+
+    void Start()
+    {
+        position = GetComponent<Transform>();
+    }
+
+    void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPoint.z = 0;
+            position.position = worldPoint;
+        }
+    }
+}
